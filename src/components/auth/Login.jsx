400b979036e2d38ba7d045bf0d3a8247f77f8a0a
mohraf <amosngok@gmail.com>
2024-02-19 20:23:25 +0300

    chore: base url

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         e.preventDefault();
 
         try{
-            const response = await axios.post('/api/portal/login', {email, password});
+            const response = await axios.post('/login', {email, password});
             localStorage.setItem('token', response.data.token);
             setEmail("");
             setPassword("");
