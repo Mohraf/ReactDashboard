284ef39e663a4fcd1d7833dd308a72197bcc6ce5
mohraf <amosngok@gmail.com>
2024-03-12 12:25:09 +0300

    feat: add employee marketing layout

--- src/pages/marketing/Employees.jsx
+++ src/pages/marketing/Employees.jsx
@@ -1,8 +1,17 @@
+import ContentWrapper from '@/components/layout/ContentWrapper'
+import SideBar from '@/components/layout/SideBar'
 import React from 'react'
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className="flex h-screen bg-gray-200 dark:bg-gray-900">
+      <SideBar />
+      <ContentWrapper>
+        <h3>Employee Broadcast</h3>
+
+        
+      </ContentWrapper>
+    </div>
   )
 }
 
