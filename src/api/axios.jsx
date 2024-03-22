7698bce40cac8736a2d2d3775a6a703d1e921977
mohraf <amosngok@gmail.com>
2024-03-22 14:31:18 +0300

    chore: safely import env variables

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8000/api/portal",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
